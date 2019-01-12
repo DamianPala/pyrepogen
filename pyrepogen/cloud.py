36c45595de83dad9c5dc754621b208db5f3e1847
Damian Pala <mail@damianpala.com>
2019-01-12 14:14:00 +0100

    Some cloud refactoring.

--- pyrepogen/cloud.py
+++ pyrepogen/cloud.py
@@ -4,7 +4,6 @@
 
 import configparser
 import ftplib
-import os
 import datetime
 from pathlib import Path
 
@@ -12,23 +11,23 @@ from . import exceptions
 from . import settings
 from . import wizard
 from . import logger
+from . import utils
 
 
 _logger = logger.get_logger(__name__)
 
 
 def upload_to_cloud(cwd='.', prompt=True):
-    _logger.info("Upload packages to the cloud server...")
-    latest_release_package_path = _get_latest_package_path(Path(cwd) / settings.DirName.RELEASE)
+    _logger.info('Upload packages to the cloud server...')
+    latest_release_package_path = utils.get_latest_file(Path(cwd) / settings.DirName.RELEASE)
 
     if prompt:
         if latest_release_package_path:
-            if not wizard.is_checkpoint_ok(__name__, "Upload the {} package?".format(latest_release_package_path),
-                                           ['y', 'n'], 'y'):
+            if not wizard.is_checkpoint_ok(__name__, f'Upload the {latest_release_package_path} package?'):
                 latest_release_package_path = None
 
     if not latest_release_package_path:
-        _logger.info("No package to upload.")
+        _logger.info('No package to upload.')
         return
 
     credentials = _read_cloud_credentials(cwd)
@@ -41,7 +40,7 @@ def upload_to_cloud(cwd='.', prompt=True):
 
 
 def list_cloud(cwd='.'):
-    _logger.info("List cloud buckets...")
+    _logger.info('List cloud buckets...')
     credentials = _read_cloud_credentials(cwd)
     with ftplib.FTP(credentials['server'], credentials['username'], credentials['password']) as ftp_conn:
         cloud_project_bucket_path = _get_cloud_project_bucket_path(credentials)
@@ -49,16 +48,16 @@ def list_cloud(cwd='.'):
         if _is_cloud_project_bucket_exists(ftp_conn, credentials):
             _print_bucket_files(ftp_conn, cloud_project_bucket_path, settings.DirName.RELEASE)
         else:
-            _logger.info("There are no buckets on the cloud server.")
+            _logger.info('There are no buckets on the cloud server.')
 
 
 def download_package(cwd='.', package_name=None):
     if not package_name:
-        package_name = input("Enter the name of the package to donwload: ")
+        package_name = input('Enter the name of the package to donwload: ')
     if settings.RELEASE_PACKAGE_SUFFIX in package_name:
         bucket = settings.DirName.RELEASE
     else:
-        raise exceptions.NameError("Incorrect package name!", logger=_logger)
+        raise exceptions.NameError('Incorrect package name!', logger=_logger)
 
     credentials = _read_cloud_credentials(cwd)
     cloud_project_bucket_path = _get_cloud_project_bucket_path(credentials)
@@ -68,7 +67,7 @@ def download_package(cwd='.', package_name=None):
         ftp_conn.cwd(bucket)
         if package_name not in ftp_conn.nlst():
             raise exceptions.FileNotFoundError(
-                "{} package not found on the cloud server".format(package_name), logger=_logger)
+                f'{package_name} package not found on the cloud server', logger=_logger)
 
         dir_where_to_download = Path(cwd) / settings.DirName.RELEASE
         if not dir_where_to_download.exists():
@@ -77,20 +76,17 @@ def download_package(cwd='.', package_name=None):
         path_where_to_download = dir_where_to_download / package_name
 
         if (path_where_to_download).exists():
-            _logger.warning("File {} already exists in {}.".format(package_name, path_where_to_download.parent))
-            _logger.info("Downloading aborted.")
+            _logger.warning(f'File {package_name} already exists in {path_where_to_download.parent}.')
+            _logger.info('Downloading aborted.')
             return
 
         with open(path_where_to_download, 'wb') as file:
-            ftp_conn.retrbinary("RETR " + package_name, file.write)
+            ftp_conn.retrbinary('RETR ' + package_name, file.write)
 
     if (path_where_to_download).exists():
-        _logger.info("File {} downloding to {} directory completeted.".format(
-            package_name, path_where_to_download.parent))
+        _logger.info(f'File {package_name} downloding to {path_where_to_download.parent} directory completeted.')
     else:
-        raise exceptions.FileNotFoundError(
-            "File {} downloading error! Please try again.".format(package_name),
-            logger=_logger)
+        raise exceptions.FileNotFoundError(f'File {package_name} downloading error! Please try again.', _logger)
 
 
 def _print_bucket_files(ftp_conn, cloud_project_bucket_path, bucket):
@@ -100,17 +96,17 @@ def _print_bucket_files(ftp_conn, cloud_project_bucket_path, bucket):
         files_in_bucket = list(ftp_conn.mlsd())
         files_in_bucket = sorted(files_in_bucket, key=lambda k: k[1]['modify'])
         if files_in_bucket:
-            _logger.info("{} bucket files:".format(bucket))
+            _logger.info(f'{bucket} bucket files:')
             for bucket_file in files_in_bucket:
-                _logger.info("{0:10} {1:10} {2} {3}".format(bucket_file[1]['unix.owner'],
+                _logger.info('{0:10} {1:10} {2} {3}'.format(bucket_file[1]['unix.owner'],
                                                             bucket_file[1]['size'],
                                                             datetime.datetime.strptime(bucket_file[1]['modify'],
                                                                                        '%Y%m%d%H%M%S'),
                                                             bucket_file[0]))
         else:
-            _logger.info("No files in bucket: {}".format(bucket))
+            _logger.info(f'No files in bucket: {bucket}')
     else:
-        _logger.info("Bucket: {} not exists on cloud server.".format(bucket))
+        _logger.info(f'Bucket: {bucket} not exists on cloud server.')
 
 
 def _is_cloud_project_bucket_exists(ftp_conn, credentials):
@@ -147,37 +143,18 @@ def _upload_package(ftp_conn, package_path, cloud_project_bucket_path, bucket):
             fh.close()
             if package_name in ftp_conn.nlst():
                 _logger.info(
-                    "File {} uploaded properly to directory {} of the cloud server!".format(
-                        package_name, ftp_conn.pwd()))
+                    f'File {package_name} uploaded properly to directory {ftp_conn.pwd()} of the cloud server!')
             else:
-                _logger.info("File {} uploading error!".format(package_name))
+                _logger.info(f'File {package_name} uploading error!')
         else:
-            _logger.info("{} already on server's bucket: {}.".format(package_path.name, ftp_conn.pwd()))
-
-
-def _get_latest_package_path(path):
-    if Path(path).exists():
-        if os.listdir(path):
-            files_list = []
-            for item in Path(path).iterdir():
-                if item.is_file():
-                    files_list.append((Path(item).stat().st_mtime, item))
-        else:
-            return None
-    else:
-        return None
-
-    return max(files_list)[1]
+            _logger.info(f"{package_path.name} already on server's bucket: {ftp_conn.pwd()}.")
 
 
 def _read_cloud_credentials(cwd='.'):
     file_path = Path(cwd) / settings.FileName.CLOUD_CREDENTIALS
 
     if not file_path.exists():
-        raise exceptions.FileNotFoundError(
-            "{} file not found!".format(
-                settings.FileName.CLOUD_CREDENTIALS),
-            logger=_logger)
+        raise exceptions.FileNotFoundError(f'{settings.FileName.CLOUD_CREDENTIALS} file not found!', _logger)
 
     with open(file_path, 'r') as file:
         properties_string = '[dummy_section]\n' + file.read()
@@ -185,16 +162,12 @@ def _read_cloud_credentials(cwd='.'):
     credentials = configparser.RawConfigParser()
     credentials.read_string(properties_string)
 
-    credentials_dict = {}
+    credentials_dict = {param: value.strip() for param, value in credentials['dummy_section'].items()}
 
-    for param in credentials['dummy_section']:
-        credentials_dict[param] = credentials['dummy_section'][param].strip()
-
-    for field in credentials_dict:
-        if not credentials_dict[field]:
+    for field, value in credentials_dict.items():
+        if not value:
             raise exceptions.CredentialsError(
-                "{} field is empty in {} file!".format(
-                    field, settings.FileName.CLOUD_CREDENTIALS), logger=_logger)
+                f'{field} field is empty in {settings.FileName.CLOUD_CREDENTIALS} file!', _logger)
 
     return credentials_dict
 
@@ -202,8 +175,7 @@ def _read_cloud_credentials(cwd='.'):
 def _create_main_bucket_tree(ftp_conn, credentials):
     if not credentials['main_bucket_path'] in ftp_conn.nlst():
         raise exceptions.BucketNotFoudError(
-            "Bucket {} not found on server!".format(
-                credentials['main_bucket_path']), logger=_logger)
+            f"Bucket {credentials['main_bucket_path']} not found on server!", _logger)
 
     ftp_conn.cwd(credentials['main_bucket_path'])
 
